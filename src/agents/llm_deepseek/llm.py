45758bd70285b56b875763fbdc8d22d875aa884f
Csmally <Csmally1221@gmail.com>
2026-07-16 20:11:13 +0800

    feat: 测试init_chat_model方法

--- src/agents/llm_deepseek/llm.py
+++ src/agents/llm_deepseek/llm.py
@@ -1,12 +1,12 @@
 import httpx
 from langchain.chat_models import init_chat_model
-from langchain_deepseek import ChatDeepSeek
 from src.utils.llm_key import deepseek_api_key, deepseek_v4_pro_model_name, deepseek_v4_flash_model_name, deepseek_llm_url
 
 # DeepSeek-v4-pro使用response_format结构化输出，在创建deepseek_llm的时候需要关闭思考模式
 
 deepseek_v4_flash_llm = init_chat_model(
     model=deepseek_v4_flash_model_name,
+    model_provider="deepseek",
     api_key=deepseek_api_key,
     base_url=deepseek_llm_url,
     extra_body={
@@ -22,6 +22,7 @@ deepseek_v4_flash_llm = init_chat_model(
 
 deepseek_v4_pro_llm = init_chat_model(
     model=deepseek_v4_pro_model_name,
+    model_provider="deepseek",
     api_key=deepseek_api_key,
     base_url=deepseek_llm_url,
     extra_body={
